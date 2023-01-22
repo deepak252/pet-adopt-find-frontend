406a104c6547d1626083b19e92f349648c7758a5
deepak252 <trojanhorsedc@gmail.com>
2023-01-22 17:19:14 +0530

    bug fix

--- lib/services/chat_socket_service.dart
+++ lib/services/chat_socket_service.dart
@@ -41,6 +41,7 @@ class ChatSocketService{
           ApiPath.baseUrl,
           IO.OptionBuilder()
               .setTransports(['websocket'])
+              .setExtraHeaders({'Connection': 'upgrade', 'Upgrade': 'websocket'})
               .disableAutoConnect()
               .build());
         
